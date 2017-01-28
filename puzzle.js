27af0823fa8b6871737c2335b39bde7863c98c2e
DaveWexler <dwexler@gmail.com>
2017-01-28 01:21:32 -0500

    added more js

---
+++ puzzle.js
@@ -0,0 +1,57 @@
+// is_moveable?
+// move_left
+// move_right
+// move_up
+// move_down
+
+var cells = document.getElementsByTagName('td')
+var empty_coords
+var coords
+
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', function(){
+    set_empty_coords()
+    if (!is_moveable(this)) {
+      return
+    } else if (is_moveable(this) === "horizontal"){
+      var spaces = parseInt(get_coords(this)[0]) - parseInt(empty_coords[0])
+      move_horizontal(this, spaces)
+    } else if (is_moveable(this) === "vertical"){
+      var spaces = parseInt(get_coords(this)[1]) - parseInt(empty_coords[1])
+      move_vertical(this, spaces)
+    }
+  })
+}
+
+function set_empty_coords() {
+  empty_coords = document.getElementsByClassName('empty')[0].id.split("-")
+}
+
+function get_coords(cell) {
+  return cell.id.split("-")
+}
+
+function is_moveable(cell) {
+  var coords = cell.id.split("-")
+  if (cell.classList[0] == "empty") {
+    return false
+  } else if (coords[0] === empty_coords[0]){
+    return "vertical"
+  } else if (coords[1] === empty_coords[1]) {
+    return "horizontal"
+  } else {return false}
+}
+
+function move_horizontal(cell, spaces) {
+
+}
+
+function move_vertical(cell, spaces) {
+  debugger
+  if (spaces === 0) {
+    cell.innerHTML("")
+    cell.className("empty")
+  } else {
+    
+  }
+}
